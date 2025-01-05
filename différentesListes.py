17854931184c2e8a4ad3bc79bee59a60aa9730ff
Elodie04 <Elodielefevre2@hotmail.com>
2025-01-05 18:32:15 +0100

    premier commit

---
+++ différentesListes.py
@@ -0,0 +1,48 @@
+# #Dictionnaire
+# fruits = {
+#     "pomme":"rouge","banane":"jaune","orange":"orange"
+# }
+
+# fruits["kiwi"] = "vert"
+
+# couleur_banane = fruits["banane"]
+
+# fruits["pomme"] = "vert"
+
+# del fruits["banane"]
+
+# print(fruits.keys())
+
+#  #Utilisation de plusieurs types de variables
+# nom = "Elodie"
+# age = 29
+# taille = 1.69
+# est_etudiant = True
+
+# print(f"Je m'appelle {nom} j'ai {age} ans, je mesure {taille} et je suis {est_etudiant} !")
+# print(type(nom))
+# print(type(age))
+# print(type(taille))
+# print(type(est_etudiant))
+
+
+# #Liste
+# fruits = [
+#     "pomme","banane", "orange"
+# ]
+
+# fruits.append ("kiwi")
+# fruits.remove ("orange")
+
+# fruits[1] = "ananas"
+
+# print("len.fruits")
+
+# fruits.sort()
+# print(fruits)
+
+#Tuple
+mon_tuple = (0, 2, 99, 45, 1, 77, 63, 41, 21)
+tuple_croissant = tuple(sorted(mon_tuple))
+print(tuple_croissant)
+
